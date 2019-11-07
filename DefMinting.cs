d09c70dc870c4eaeb7a874c8051fd626f9aec33f
sircapted <38248493+sircapted@users.noreply.github.com>
2019-11-06 18:08:08 -0600

    Add files via upload
    take gold ingots and make coins

---
+++ DefMinting.cs
@@ -0,0 +1,194 @@
+using System;
+
+using Server;
+using Server.Items;
+
+namespace Server.Engines.Craft
+{
+    public class DefMinting : CraftSystem
+    {
+        public override SkillName MainSkill
+        {
+            get { return SkillName.Blacksmith; }
+        }
+
+        public override string GumpTitleString { get { return "<BASEFONT COLOR=#FFFFFF SIZE=4><CENTER>MINTING MENU</CENTER></BASEFONT>"; } }
+
+        private static CraftSystem m_CraftSystem;
+
+        public static CraftSystem CraftSystem
+        {
+            get
+            {
+                if (m_CraftSystem == null)
+                    m_CraftSystem = new DefMinting();
+
+                return m_CraftSystem;
+            }
+        }
+
+        public override CraftECA ECA { get { return CraftECA.ChanceMinusSixtyToFourtyFive; } }
+
+        public override double GetChanceAtMin(CraftItem item)
+        {
+            return 0.0; // 0%
+        }
+
+        private DefMinting()
+            : base(1, 1, 1.25)
+        {
+        }
+
+        private static Type typeofAnvil = typeof(AnvilProperty_Minting);
+        private static Type typeofForge = typeof(ForgeProperty_Minting);
+
+        public static void CheckAnvilAndForge(Mobile from, int range, out bool anvil, out bool forge)
+        {
+            anvil = false;
+            forge = false;
+
+            Map map = from.Map;
+
+            if (map == null)
+                return;
+
+            IPooledEnumerable eable = map.GetItemsInRange(from.Location, range);
+
+            foreach (Item item in eable)
+            {
+                Type type = item.GetType();
+
+                bool isAnvil = (type.IsDefined(typeofAnvil, false) || item.ItemID == 4015 || item.ItemID == 4016 || item.ItemID == 0x2DD5 || item.ItemID == 0x2DD6);
+                bool isForge = (type.IsDefined(typeofForge, false) || item.ItemID == 4017 || (item.ItemID >= 6522 && item.ItemID <= 6569) || item.ItemID == 0x2DD8);
+
+                if (isAnvil || isForge)
+                {
+                    if ((from.Z + 16) < item.Z || (item.Z + 16) < from.Z || !from.InLOS(item))
+                        continue;
+
+                    anvil = anvil || isAnvil;
+                    forge = forge || isForge;
+
+                    if (anvil && forge)
+                        break;
+                }
+            }
+
+            eable.Free();
+
+            for (int x = -range; (!anvil || !forge) && x <= range; ++x)
+            {
+                for (int y = -range; (!anvil || !forge) && y <= range; ++y)
+                {
+                    StaticTile[] tiles = map.Tiles.GetStaticTiles(from.X + x, from.Y + y, true);
+
+                    for (int i = 0; (!anvil || !forge) && i < tiles.Length; ++i)
+                    {
+                        int id = tiles[i].ID;
+
+                        bool isAnvil = (id == 4015 || id == 4016 || id == 0x2DD5 || id == 0x2DD6);
+                        bool isForge = (id == 4017 || (id >= 6522 && id <= 6569) || id == 0x2DD8);
+
+                        if (isAnvil || isForge)
+                        {
+                            if ((from.Z + 16) < tiles[i].Z || (tiles[i].Z + 16) < from.Z || !from.InLOS(new Point3D(from.X + x, from.Y + y, tiles[i].Z + (tiles[i].Height / 2) + 1)))
+                                continue;
+
+                            anvil = anvil || isAnvil;
+                            forge = forge || isForge;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override int CanCraft(Mobile from, BaseTool tool, Type itemType)
+        {
+            if (tool == null || tool.Deleted || tool.UsesRemaining < 0)
+                return 1044038; // You have worn out your tool!
+            else if (!BaseTool.CheckTool(tool, from))
+                return 1048146; // If you have a tool equipped, you must use that tool.
+            else if (!BaseTool.CheckAccessible(tool, from))
+                return 1044263; // The tool must be on your person to use.
+
+            bool anvil, forge;
+            CheckAnvilAndForge(from, 2, out anvil, out forge);
+
+            if (anvil && forge)
+                return 0;
+
+            return 1044267; // You must be near an anvil and a forge to smith items.
+        }
+
+        public override void PlayCraftEffect(Mobile from)
+        {
+            from.PlaySound(0x2A);
+        }
+
+        // Delay to synchronize the sound with the hit on the anvil
+        private class InternalTimer : Timer
+        {
+            private Mobile m_From;
+
+            public InternalTimer(Mobile from)
+                : base(TimeSpan.FromSeconds(0.7))
+            {
+                m_From = from;
+            }
+
+            protected override void OnTick()
+            {
+                m_From.PlaySound(0x2A);
+            }
+        }
+
+        public override int PlayEndingEffect(Mobile from, bool failed, bool lostMaterial, bool toolBroken, int quality, bool makersMark, CraftItem item)
+        {
+            if (toolBroken)
+                from.SendLocalizedMessage(1044038); // You have worn out your tool
+
+            if (failed)
+            {
+                if (lostMaterial)
+                    return 1044043; // You failed to create the item, and some of your materials are lost.
+                else
+                    return 1044157; // You failed to create the item, but no materials were lost.
+            }
+            else
+            {
+                if (quality == 0)
+                    return 502785; // You were barely able to make this item.  It's quality is below average.
+                else if (makersMark && quality == 2)
+                    return 1044156; // You create an exceptional quality item and affix your maker's mark.
+                else if (quality == 2)
+                    return 1044155; // You create an exceptional quality item.
+                else
+                    return 1044154; // You create the item.
+            }
+        }
+
+        public override void InitCraftList()
+        {
+            AddCraft(typeof(Gold), "Gold", "Gold Coin", 12.0, 62.0, typeof(GoldIngot), 1027146, 2, 1044037); //1027146 = gold ingots
+
+            Resmelt = true;
+            //Repair = false;
+            //MarkOption = false;
+            //CanEnhance = Core.AOS;
+        }
+    }
+
+    public class ForgeProperty_Minting : Attribute
+    {
+        public ForgeProperty_Minting()
+        {
+        }
+    }
+
+    public class AnvilProperty_Minting : Attribute
+    {
+        public AnvilProperty_Minting()
+        {
+        }
+    }
+}
